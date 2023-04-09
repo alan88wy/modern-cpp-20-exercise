46fcb862a6a482377b9996a752c2d98ed0ba8af4
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-04-09 11:49:45 +0800

    Initial Commit

---
+++ a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+consteval int get_value()
+{
+    return 5;
+}
+
+int main() {
+	cout << "Hello World!" << endl;
+
+	constexpr int value = get_value();
+
+	cout << "Value : " << value << endl;
+}

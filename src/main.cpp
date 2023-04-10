ff03c9ae9746560402e4ea1f0e6a3caca0a9cede
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-04-10 21:38:11 +0800

    Added modulus operator

--- src/main.cpp
+++ src/main.cpp
@@ -24,11 +24,35 @@ int main(int argc, char **argv) {
 	// cin >> age;
 	// cout << "Hello " << name << ". You are " << age << " years old!" << endl;
 
-	int i {20};
-	int j {30};
-	int k { i + j };
+	// Create variables and initialize variables
+	// int i {20};
+	// int j {30};
+	// int k { i + j };
 
-	cout << i << " + " << j << " = " << k << endl;
+	// cout << i << " + " << j << " = " << k << endl;
+
+	// char a { 'a'};
+	// char b {'b'};
+
+	// cout << a << " " << b << endl;
+
+	// Using auto to create variables
+
+	// auto c {'c'};
+	// auto d {13.2};
+	// auto e {14.5f};
+
+    // c = 'd';
+
+	// cout << c << " " << d << " " << e << endl;
+
+	// Data Operations
+
+	// Modulus operator
+	cout << 33 % 10 << endl;
+
+	
+
 
 	return 0;
 }

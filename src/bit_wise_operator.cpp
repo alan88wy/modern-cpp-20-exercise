9c16714f159cc6667a9bed8708d89c5076d16626
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-04-13 16:25:09 +0800

    Moved masks to head of program

--- src/bit_wise_operator.cpp
+++ src/bit_wise_operator.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+const unsigned char mask_bit_0 {0b00000001};
+const unsigned char mask_bit_1 {0b00000010};
+const unsigned char mask_bit_2 {0b00000100};
+const unsigned char mask_bit_3 {0b00001000};
+const unsigned char mask_bit_4 {0b00010000};
+const unsigned char mask_bit_5 {0b00100000};
+const unsigned char mask_bit_6 {0b01000000};
+const unsigned char mask_bit_7 {0b10000000};
+
 int main(int argc, char  **argv) {
 
     unsigned short int data {0b11111110};
@@ -99,16 +108,6 @@ int main(int argc, char  **argv) {
     cout << "Masks" << endl;
     cout << "=====" << endl;
     cout << endl;
-
-    // auto mask = 0b000000100;
-    const unsigned char mask_bit_0 {0b00000001};
-    const unsigned char mask_bit_1 {0b00000010};
-    const unsigned char mask_bit_2 {0b00000100};
-    const unsigned char mask_bit_3 {0b00001000};
-    const unsigned char mask_bit_4 {0b00010000};
-    const unsigned char mask_bit_5 {0b00100000};
-    const unsigned char mask_bit_6 {0b01000000};
-    const unsigned char mask_bit_7 {0b10000000};
 
     unsigned short int var1 {0b01100001};
 

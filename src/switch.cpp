c2725f0641737f8857359a336000f9c923429db3
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-04-13 21:35:52 +0800

    Added switch

---
+++ src/switch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    switch (1) {
+        case 1: {
+            cout << "1" << endl;
+            
+            }
+            break;
+        default:
+            cout << "default" << endl;
+            
+
+    }
+}

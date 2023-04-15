0972db7792ead84589ff3beb6af20c780f9dc26e
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-04-15 16:27:23 +0800

    added comma operator

---
+++ src/comma_operator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int increment {5};
+    int number1 {10};
+    int number2 {20};
+    int number3 {25};
+
+    int result = (number1 *= ++increment, number2 - (++increment), number3 += ++increment);
+
+    cout << "Number 1 : " << number1 << endl;  // 60
+    cout << "Number 2 : " << number2 << endl;  // 20; no change since the result is not store back to number2
+    cout << "Number 3 : " << number3 << endl;  // 33
+    cout << "Result : " << result << endl;     // 33
+
+    return 0;
+}

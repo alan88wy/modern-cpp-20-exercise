51bf4779e646b772450585b2d7a13e69aaa76aa3
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-04-24 11:35:24 +0800

    Added comment

--- src/character_manipulation.cpp
+++ src/character_manipulation.cpp
@@ -9,20 +9,20 @@ int main() {
 
     cout << boolalpha << endl;
     cout << "c        : " << c << endl;
-    cout << "isalnum  : " << (bool) isalnum(c) << endl;
-    cout << "isalpha  : " << (bool) isalpha(c) << endl;
-    cout << "isblank  : " << (bool) isblank(c) << endl;
-    cout << "iscntrl  : " << (bool) iscntrl(c) << endl;
-    cout << "isdigit  : " << (bool) isdigit(c) << endl;
-    cout << "isgraph  : " << (bool) isgraph(c) << endl;
-    cout << "islower  : " << (bool) islower(c) << endl;
-    cout << "isprint  : " << (bool) isprint(c) << endl;
-    cout << "ispunct  : " << (bool) ispunct(c) << endl;
-    cout << "isspace  : " << (bool) isspace(c) << endl;
-    cout << "isupper  : " << (bool) isupper(c) << endl;
-    cout << "isxdigit : " << (bool) isxdigit(c) << endl;
-    cout << "tolower  : " << (char) tolower(c) << endl;
-    cout << "toupper  : " << (char) toupper(c) << endl;
+    cout << "isalnum  : " << (bool) isalnum(c) << endl;  // checks if a character is alphanumeric
+    cout << "isalpha  : " << (bool) isalpha(c) << endl;  // checks if a character is alphabetic
+    cout << "isblank  : " << (bool) isblank(c) << endl;  // checks if a character is a blank character
+    cout << "iscntrl  : " << (bool) iscntrl(c) << endl;  // checks if a character is a control character
+    cout << "isdigit  : " << (bool) isdigit(c) << endl;  // checks if a character is a digit
+    cout << "isgraph  : " << (bool) isgraph(c) << endl;  // checks if a character is a graphical character
+    cout << "islower  : " << (bool) islower(c) << endl;  // checks if a character is lowercase
+    cout << "isprint  : " << (bool) isprint(c) << endl;  // checks if a character is a printing character
+    cout << "ispunct  : " << (bool) ispunct(c) << endl;  // checks if a character is a punctuation character
+    cout << "isspace  : " << (bool) isspace(c) << endl;  // checks if a character is a space character
+    cout << "isupper  : " << (bool) isupper(c) << endl;  // checks if a character is an uppercase character
+    cout << "isxdigit : " << (bool) isxdigit(c) << endl; // checks if a character is a hexadecimal character
+    cout << "tolower  : " << (char) tolower(c) << endl;  // converts a character to lowercase
+    cout << "toupper  : " << (char) toupper(c) << endl;  // converts a character to uppercase
 
     return 0;
 }

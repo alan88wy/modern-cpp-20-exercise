672e0a6cc41b12faa9cfcf88038d2326d06b4caf
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-04-25 22:55:48 +0800

    Added string examples

--- src/string.cpp
+++ src/string.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 
 using namespace std;
 
@@ -38,6 +39,14 @@ int main() {
                 memmove	Move block of memory (function)
                 strcpy	Copy string (function)
                 strncpy	Copy characters from string (function)
+                        char * strncpy ( char * destination, const char * source, size_t num );
+                        destination
+                            Pointer to the destination array where the content is to be copied.
+                        source
+                            C string to be copied.
+                        num
+                            Maximum number of characters to be copied from source.
+                            size_t is an unsigned integral type.
 
             Concatenation:
                 strcat	Concatenate strings (function)
@@ -81,6 +90,7 @@ int main() {
 
     char str1 [] { "A fox jumping over the fence !"};
     char str2 [] { "Two dog jump over the fence !"};
+    char str3 [] { " Life's is good !" };
     char target { 'o' };
 
     cout << str1 << endl;
@@ -92,7 +102,6 @@ int main() {
     cout << "strchr  : " << strchr(str1, target) << endl; 
     cout << "strstr  : " << strstr(str1, "fox") << endl;             // Locate "fox" in the string and return string
                                                                      // from that position onwards.
-
     char * tok;
 
     /*
@@ -102,6 +111,7 @@ int main() {
         In subsequent calls, the function expects a null pointer and uses the position right 
         after the end of the last token as the new starting location for scanning.
     */
+
     tok = strtok(str1, " ");
 
     cout << "strtok  : " << endl;
@@ -114,7 +124,144 @@ int main() {
         tok = strtok(NULL, " ");
     }
 
-    
+    cout << "strcat : " << strcat(str2, str3) << endl;
+    char * dest = new char[strlen(str3) + 1];
+    dest = strcpy(dest, str3);
+    cout << "strcpy : " << dest << endl;
+    cout << "strncpy : " << strncpy(dest, str2, strlen(dest)) << endl;
+
+    // Get "the sky is blue" from the sentences below.
+
+    const char src1[] {"will old space the replace"};
+    const char src2[] {"sense sky  hit has"};
+    const char src3[] {"hello went is get"};
+    const char src4[] {"red blue yellow orange"};
+
+    char* result;
+    result = new char[20];
+
+    strncpy(result, src1 + 15, 3);
+    strncpy(result + 3,src2 + 5, 4 );
+    strncpy(result + 7, src3 + 10, 3);
+
+    strncpy(result + 10,src4 + 3, 5 );
+
+    result[15] = '\0';
+    cout << "result : " << result << endl << endl;
+
+    /*
+        String
+        ******
+
+        Member functions
+            (constructor)	Construct string object (public member function)
+            (destructor)	String destructor (public member function)
+            operator=	String assignment (public member function)
+
+        Iterators:
+            begin	Return iterator to beginning (public member function)
+            end	    Return iterator to end (public member function)
+            rbegin	Return reverse iterator to reverse beginning (public member function)
+            rend	Return reverse iterator to reverse end (public member function)
+            cbegin	Return const_iterator to beginning (public member function)
+            cend	Return const_iterator to end (public member function)
+            crbegin	Return const_reverse_iterator to reverse beginning (public member function)
+            crend	Return const_reverse_iterator to reverse end (public member function)
+
+        Capacity:
+            size	        Return length of string (public member function)
+            length	        Return length of string (public member function)
+            max_size	    Return maximum size of string (public member function)
+            resize	        Resize string (public member function)
+            capacity	    Return size of allocated storage (public member function)
+            reserve	        Request a change in capacity (public member function)
+            clear	        Clear string (public member function)
+            empty	        Test if string is empty (public member function)
+            shrink_to_fit	Shrink to fit (public member function)
+
+        Element access:
+            operator[]	    Get character of string (public member function)
+            at	            Get character in string (public member function)
+            back	        Access last character (public member function)
+            front	        Access first character (public member function)
+
+        Modifiers:
+            operator+=	    Append to string (public member function)
+            append	        Append to string (public member function)
+            push_back	    Append character to string (public member function)
+            assign	        Assign content to string (public member function)
+            insert	        Insert into string (public member function)
+            erase	        Erase characters from string (public member function)
+            replace	        Replace portion of string (public member function)
+            swap	        Swap string values (public member function)
+            pop_back	    Delete last character (public member function)
+
+        String operations:
+            c_str	            Get C string equivalent (public member function)
+        data	                Get string data (public member function)
+            get_allocator   	Get allocator (public member function)
+            copy        	    Copy sequence of characters from string (public member function)
+            find        	    Find content in string (public member function)
+            rfind       	    Find last occurrence of content in string (public member function)
+            find_first_of   	Find character in string (public member function)
+            find_last_of    	Find character in string from the end (public member function)
+            find_first_not_of	Find absence of character in string (public member function)
+            find_last_not_of	Find non-matching character in string from the end (public member function)
+            substr	            Generate substring (public member function)
+            compare	            Compare strings (public member function)
+
+        Member constants
+            npos	Maximum value for size_t (public static member constant)
+
+        Non-member function overloads
+            operator+   	Concatenate strings (function)
+        relational          operators	Relational operators for string (function)
+            swap	        Exchanges the values of two strings (function)
+            operator>>	    Extract string from stream (function)
+            operator<<	    Insert string into stream (function)
+            getline	        Get line from stream into string (function)
+    */
+   
+    string full_name; // Empty String
+    string planet {"Earth. Where the sky is blue"};  // Initialized with string literal
+    string preferred_planet {planet}; // Initialized with other string
+    string message {"Hello there", 5}; // Initialized with part of string literal
+    string weird_message(4, 'e'); // Initialized with multiple copies of a char
+    string greeting {"Hello World"};
+    string say_hello {greeting, 6, 5}; // Initialize with part of existing string (start at Index 6, take 5 characters)
+                                       // result = World
+    cout << "String exercise : " << endl << endl;
+    cout << "Full Name : " << full_name << endl;
+    cout << "Planet : " << planet << endl;
+    cout << "Preferred Planet : " << preferred_planet << endl;
+    cout << "message : " << message << endl;
+    cout << "greeting : " << greeting << endl;
+    cout << "Saying Hello : " << say_hello << endl << endl;
+
+    cout << "Concatenate message + say_hello : " << message + " " + say_hello << endl << endl;
+
+    // Using string literals
+    cout << "Using string literals :" << endl << endl;
+    using namespace string_literals;
+
+    string str5 {"Hello"s + " World!"}; // turn both into strings using the s suffix
+    cout << "str5 : " << str5 << endl;
+
+    str5.append(" Welcome.");
+
+    cout << "str5.append(Welcome) : " << str5 << endl;
+
+    // can append multiple character
+    str5.append(10, '!');
+
+    cout << "str5.append(! 10 times) : " << str5 << endl;
+
+    // Append substring
+    str5.append("Hello World", 0, 5);
+
+    cout << "str5.append('Hello World', 0, 5) : " << str5 << endl;
+
+    cout << "to_string(55) + ' Hello' : " << to_string(55) + " Hello" << endl;
 
     return 0;
 }

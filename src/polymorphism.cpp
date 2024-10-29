5916185e6350af7853b612352e2c3177066fcfaf
Alan Wan <alan88w@yahoo.com>
2024-10-29 21:54:04 +0800

    added polymorphism

---
+++ src/polymorphism.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+class Base{
+    public:
+        virtual void doSomething()
+        {
+            std::cout << "I am doing Something!\n";
+        };
+};
+
+// This cannot be instantiate
+class PureBase
+{
+    public:
+
+        virtual void purelyDoSomething() = 0;  // Pure virtual function = interface
+};
+
+class Ipolymorphic: public Base, public PureBase
+{
+    public:
+        void doSomething()
+        {
+            std::cout << "I had polymorphic into something else!\n";
+        };
+
+        void purelyDoSomething()
+        {
+            std::cout << "I am build from interface!\n";
+        };
+};
+
+int main()
+{
+    Ipolymorphic me;
+
+    me.doSomething();
+
+    me.purelyDoSomething();
+
+    Base myBase;
+
+    myBase.doSomething();
+
+    // PureBase myPureBase;  // This will fail with this error: cannot declare variable 'myPureBase' to be of abstract type 'PureBase'
+
+    return 1;
+}
+

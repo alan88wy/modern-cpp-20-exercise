7aaab1da8e736e3d6f56c25a2d565c934cf0216c
Alan Wan <alan88w@yahoo.com>
2025-04-26 10:54:57 +0800

    Added comment

--- src/exception_handling.cpp
+++ src/exception_handling.cpp
@@ -81,3 +81,39 @@ int main()
 
 	println("{} ", myInts);
 }
+
+/*
+Standard exceptions
+*******************
+* logic_error
+  - invalid_argument
+  - domain_error
+  - length_error
+  - out_of_range
+  - future_error (since C++11)
+
+* runtime_error
+  - range_error
+  - overflow_error
+  - underflow_error
+  - regex_error (since C++11)
+  - system_error (since C++11)
+    # ios_base::failure (since C++11)
+    # filesystem::filesystem_error (since C++17)
+  - tx_exception (TM TS)
+  - nonexistent_local_time (since C++20)
+  - ambiguous_local_time (since C++20)
+  - format_error (since C++20)
+* bad_typeid
+* bad_cast
+  - bad_any_cast (since C++17)
+* bad_optional_access (since C++17)
+* bad_expected_access (since C++23)
+* bad_weak_ptr (since C++11)
+* bad_function_call (since C++11)
+* bad_alloc
+  - bad_array_new_length (since C++11)
+* bad_exception
+* ios_base::failure (until C++11)
+* bad_variant_access (since C++17)
+*/

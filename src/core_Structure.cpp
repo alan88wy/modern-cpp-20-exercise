650b0de7792694ff20ac7680ee572055e0c297e6
Alan Wan <alan88w@yahoo.com>
2025-06-25 21:03:39 +0800

    added function to struct

--- src/core_Structure.cpp
+++ src/core_Structure.cpp
@@ -4,12 +4,15 @@ struct Employee {
 	int id;
 	const char* name;
 	const char* role;
+	const char* get_name() {
+		return this->name;
+	};
 };
 
 int main() {
 	Employee joe = { 42, "Joe", "Boss" };
 
-	printf("%s is the %s and has id %d \n", joe.name, joe.role, joe.id);
+	printf("%s is the %s and has id %d \n", joe.get_name(), joe.role, joe.id);
 
 	return 0;
 }

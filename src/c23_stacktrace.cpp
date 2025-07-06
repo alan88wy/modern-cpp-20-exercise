8f3b924bd840bed6a777f2789633ff18d2b00d69
Alan Wan <alan88w@yahoo.com>
2025-07-06 22:15:16 +0800

    Added C++23 Stacktrace example

---
+++ src/c23_stacktrace.cpp
@@ -0,0 +1,39 @@
+import std;
+
+using namespace std;
+
+void handleStackTrace(const stacktrace& trace)
+{
+    println("   Stack trace information:");
+    println("      There are {} frames in the stack trace.", trace.size());
+    println("   Here are all the frames in the stack trace:");
+    println("----------------------------------------------------------------------------------------");
+    println("{}", trace);
+    println("----------------------------------------------------------------------------------------");
+
+}
+
+void C()
+{
+    println("Entered C().");
+    handleStackTrace(stacktrace::current());
+    println("Exiting C().");
+}
+
+void B() {
+    println("Entered B().");
+    C();
+    println("Exiting B().");
+}
+
+void A()
+{
+    println("Entered A().");
+    B();
+    println("Exiting A().");
+}
+
+int main()
+{
+    A();
+}

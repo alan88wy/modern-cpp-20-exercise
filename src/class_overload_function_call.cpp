c307b95a7d885bb7eb4d44cb3c87de0ffcf5993a
Alan Wan <alan88w@yahoo.com>
2025-10-01 14:38:14 +0800

    Added function call overloading example

---
+++ src/class_overload_function_call.cpp
@@ -0,0 +1,36 @@
+import std;
+
+using namespace std;
+
+class Squarer
+{
+public:
+    int operator()(int value) const; // Overloaded function call operator. 
+    int doSquare(int value) const; // Normal member function.
+};
+// Implementation of overloaded function call operator. 
+int Squarer::operator()(int value) const { return doSquare(value); } 
+// Implementation of normal member function.
+int Squarer::doSquare(int value) const { return value * value; }
+
+int main()
+{
+    Squarer squarer;
+    int number = 3;
+
+    // Using the overloaded function call operator
+    int result1 = squarer(number);
+    cout << "Using overloaded operator(): " << result1 << endl;
+
+    // Using the normal member function
+    int result2 = squarer.doSquare(number);
+    cout << "Using normal member function doSquare(): " << result2 << endl;
+
+    // Call the function call operator.
+    int xSquared { squarer(number) };
+    int xSquaredAgain { squarer.doSquare(xSquared) };
+    // Call the normal member function. 
+    println("{} squared is {}, and squared again is {}.", number, xSquared, xSquaredAgain);
+
+    return 0;
+}
